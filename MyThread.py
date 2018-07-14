7a20277892ead8b2c34d83aa13dc0e6e46b1a132
flag625 <liangzhisen@outlook.com>
2018-07-14 16:06:25 +0800

    创建Thread子类

---
+++ MyThread.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import threading
+from time import ctime
+
+class MyThread(threading.Thread):
+    def __init__(self, func, args, name=''):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def getResult(self):
+        return self.res
+
+    def run(self):
+        print('starting %d at: %s' %(self.name, ctime()))
+        self.res = self.func(*self.args)
+        print('%s finished at: %s' %(self.name, ctime()))
